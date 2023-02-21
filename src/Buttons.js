0074d02a43856c1d57b0f02f2c2de21088577f33
“Valerij-Hering” <zell9113@gmail.com>
2023-02-21 22:02:01 +0100

    first commit

--- src/Buttons.js
+++ src/Buttons.js
@@ -1,8 +1,8 @@
 const Buttons = ({chosenTrainings}) => {
     return (
         <div className="filterBtn-box">
-            <button className="filter-btn" onClick={() => chosenTrainings('novice')}>Novice</button>
-            <button className="filter-btn" onClick={() => chosenTrainings('profe')}>Profe</button>
+            <button className="filter-btn-left" onClick={() => chosenTrainings('novice')}>Novice</button>
+            <button className="filter-btn-right" onClick={() => chosenTrainings('profe')}>Profe</button>
         </div>
     )
 }
